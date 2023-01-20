f36f75143e96e136ca8ae91b3436ec13274f992b
NathanaelBlackbourn <nathanaelblackbourn@gmail.com>
2023-01-20 11:45:43 +0100

    gameEntity, movingEntity and player classes created.

---
+++ src/movingEntity.ts
@@ -0,0 +1,10 @@
+abstract class MovingEntity extends GameEntity {
+    private speed: number;
+
+    constructor (position: p5.Vector, size: p5.Vector, speed: number) {
+        super(position, size);
+        this.speed = speed;
+    }
+
+    abstract draw(): void;
+}
--- src/player.ts
+++ src/player.ts
@@ -1,1 +1,20 @@
-class Player {}
+/// <reference path="movingEntity.ts"/>
+
+class Player extends MovingEntity {
+  constructor(x: number, y: number, cellSize: number) {
+    super(
+      new p5.Vector(
+        x * cellSize + cellSize * 0.15,
+        y * cellSize + cellSize * 0.15
+      ),
+      new p5.Vector(cellSize * 0.7, cellSize * 0.7),
+      10
+    );
+  }
+
+  draw() {
+    push();
+    fill("blue");
+    rect(this.position.x, this.position.y, this.size.x);
+  }
+}
