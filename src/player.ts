e4944b975c917d9c67eff786621a48f3410d274f
NathanaelBlackbourn <nathanaelblackbourn@gmail.com>
2023-01-20 15:38:59 +0100

    Both players moving by respective keys.

--- src/player.ts
+++ src/player.ts
@@ -14,11 +14,16 @@ class Player extends MovingEntity {
    */
   private powerupTimer: number;
 
+  private leftButton!: number;
+  private rightButton!: number;
+  private upButton!: number;
+  private downButton!: number;
+
   public constructor(position: p5.Vector, cellSize: number, playerNumber: number) {
     super(
       position,
       new p5.Vector(cellSize * 0.7, cellSize * 0.7),
-      10
+      7
     );
     this.position.x += cellSize * 0.15;
     this.position.y += cellSize * 0.15;
@@ -29,9 +34,23 @@ class Player extends MovingEntity {
     this.isInverted = false;
     this.powerupTimer = 0;
 
+    if (playerNumber === 1) {
+      this.leftButton = 65;
+      this.rightButton = 68;
+      this.upButton = 87;
+      this.downButton = 83;
+    } else if (playerNumber === 2) {
+      this.leftButton = LEFT_ARROW;
+      this.rightButton = RIGHT_ARROW;
+      this.upButton = UP_ARROW;
+      this.downButton = DOWN_ARROW;
+    }
+
   }
 
-  public update() {};
+  public update() {
+    this.checkUserInput();
+  };
 
   public draw() {
     push();
@@ -43,7 +62,22 @@ class Player extends MovingEntity {
   /**
    * Called from update. Checks keyboard input.
    */
-  private checkUserInput() {};
+  private checkUserInput() {
+
+    if (keyIsDown(this.leftButton)) {
+      this.position.x -= this.speed;
+    }
+    if (keyIsDown(this.rightButton)) {
+      this.position.x += this.speed;
+    }
+    if (keyIsDown(this.upButton)) {
+      this.position.y -= this.speed;
+    }
+    if (keyIsDown(this.downButton)) {
+      this.position.y += this.speed;
+    }
+
+  };
 
   /**
    * Sets player to frozen, sets time limit.
