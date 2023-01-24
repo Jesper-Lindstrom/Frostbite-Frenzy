e15f0e878901f1f5b68731a774be734536759109
edvindjulic <djulicedvin@gmail.com>
2023-01-24 10:05:26 +0100

    Code for Menu + MenuPage. Removed PageContent.

--- src/menu.ts
+++ src/menu.ts
@@ -2,52 +2,16 @@ class Menu {
   private activePage: MenuPage;
 
   public constructor() {
-    this.activePage = new StartPage();
+      this.activePage = new StartPage();
   }
 
-  public update() {}
+  public update(): void {}
 
-  public draw() {
-    this.activePage.draw();
+  public draw(): void {
+      this.activePage.draw();
   }
 
-  public openPage(page: MenuPage) {
-    this.activePage = page;
+  public openPage(page: MenuPage): void {
+      this.activePage = page;
   }
 }
-
-
-
-/* class Menu {
-  private startPage: StartPage;
-  private objectives: Objectives;
-  private controls: Controls;
-  private gameOver: GameOver;
-
-  public constructor() {
-    this.startPage = new StartPage;
-    this.objectives = new Objectives;
-    this.controls = new Controls;
-    this.gameOver = new GameOver;
-  }
-
-
-  update() {}
-
-
-  public draw() {
-    this.drawStartPage();
-    this.drawObjectives();
-    this.drawControls();
-    this.drawGameOver();
-  }
-
-  public drawStartPage() {};
-
-  public drawObjectives() {};
-  
-  public drawControls() {};
-
-  public drawGameOver() {};
-
-} */
--- src/menupage.ts
+++ src/menupage.ts
@@ -1,52 +1,12 @@
 class MenuPage {
-    public constructor() {}
-
-    public draw() {}
-    
-}
-
-
-
-
-
-
-
-
-
-
-
-/* interface PageContent {
-    [key: string]: string;
-}
-
-// New example
-export class MenuPage {
-    private content: PageContent;
-
-    public constructor(content: PageContent) {
-        this.content = content;
-    }
-
     public draw(): void {
-        // Calls for the drawImage-method
-        this.drawImages();
         this.drawText();
+        this.drawImages();
         this.drawShapes();
     }
 
-    public drawText(): void {
-        // Draw the text
-    }
+    protected drawImages(): void {}
+    protected drawText(): void {}
+    protected drawShapes(): void {}
+}
 
-    public drawImages(): void {
-        // Draw the images
-    }
-
-    public drawShapes(): void {
-        // Draw the images
-    }
-
-    protected openPage(page: MenuPage): void {
-        // Logic for Opening a page 
-    }
-}   */
