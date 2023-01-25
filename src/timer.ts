215bf03faa6fd975e0d77d0b16b7471aed69ea5c
NathanaelBlackbourn <nathanaelblackbourn@gmail.com>
2023-01-25 09:45:06 +0100

    Timer frame created.

--- src/timer.ts
+++ src/timer.ts
@@ -1,1 +1,39 @@
-class Timer {}
+class Timer {
+    private minutes: number;
+    private seconds: number;
+    timerRunning: boolean;
+
+    public constructor() {
+        this.minutes = 3;
+        this.seconds = 0;
+        this.timerRunning = false;
+    }
+
+    /**
+     * Updates the time every second using delta time.
+     */
+    public update() {
+
+    }
+
+    /**
+     * Updates the time display every second.
+     */
+    public draw() {
+
+    }
+
+    /**
+     * Called by Game to control spawning of Powerups and the game end.
+     */
+    public getTime() {
+
+    }
+
+    /**
+     * Starts the timer once the game is loaded.
+     */
+    public runTimer() {
+
+    }
+}
