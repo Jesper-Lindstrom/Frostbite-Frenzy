b880f3562c4f2d597995a6ba16096ff955204d28
NathanaelBlackbourn <nathanaelblackbourn@gmail.com>
2023-01-31 10:22:25 +0100

    Layout complete with scores.

--- src/gameover.ts
+++ src/gameover.ts
@@ -12,6 +12,9 @@ class Gameover extends MenuPage {
   private options: string[] = ["New Game", "Scores"];
   private buttons: p5.Element[] = [];
 
+  private winnerScore: number;
+  private loserScore: number;
+
 
   public constructor(menu: Menu) {
     super()
@@ -24,10 +27,10 @@ class Gameover extends MenuPage {
     this.key2 = images.key2;
     this.key3 = images.key3;
     this.key4 = images.key4;
-    this.drawShapes();
-    this.drawText();
-    this.drawImages();
     this.drawButtons();
+
+    this.winnerScore = 2;
+    this.loserScore = 3;
   }
 
   public drawShapes() {
@@ -35,28 +38,20 @@ class Gameover extends MenuPage {
     strokeWeight(5); // frame thickness
     stroke(255, 255, 255); // set the "frame" color
     fill(74, 122, 167);
-    rectMode(CENTER);
-    rect(width / 2, height / 2, 700, 600); // "frame"
-    line(1033, 350, 338, 350);
+    rect(this.x, this.y, this.width, this.height); // "frame"
+    line(this.x, height / 2, this.width + this.x, height /2);
     pop(); // restore previous styles and transformations
   }
 
   protected drawText() {
-
-    // push();
-    // textFont("Sansita");
-    // fill(173, 202, 220);
-    // textSize(31);
-    // textAlign(CENTER, CENTER);
-    // text("Congratulations!!", (width / 2), height / 8);
-    // pop();
 
     push(); // save current styles and transformations
     textFont("Freckle Face");
     fill(255);
     textSize(50);
     textAlign(CENTER, CENTER);
-    text("The winner!!", width / 2, height / 5.5);
+    text("The winner!!", width / 2, this.y + this.height * 0.05);
+    text(this.winnerScore, width / 2, this.y + this.height * 0.4);
     pop(); // restore previous styles and transformations
 
     push(); // save current styles and transformations
@@ -64,45 +59,31 @@ class Gameover extends MenuPage {
     fill(255, 0, 0);
     textSize(50);
     textAlign(CENTER, CENTER);
-    text("Defeated!", width / 2, height / 1.8);
+    text("Defeated!", width / 2, this.y + this.height * 0.55);
+    text(this.loserScore, width / 2, this.y + this.height * 0.9);
     pop(); // restore previous styles and transformations
 
 }
 
 public drawImages() {
+  push();
   imageMode(CENTER);
-  if (this.yellowSnowman.width > 0 && this.yellowSnowman.height > 0) {
-    image(this.yellowSnowman, width / 2, height / 2 - 180, 80, 100);
-  }
-  if (this.greenSnowman.width > 0 && this.greenSnowman.height > 0) {
-    image(this.greenSnowman, width / 2, height / 2 - -80, 80, 100);
-  }
-  if (this.key.width > 0 && this.key.height > 0) {
-    image(this.key, width / 2 - -180, height / 2 - 190, 70, 50);
-  }
-  if (this.key2.width > 0 && this.key2.height > 0) {
-    image(this.key2, width / 2 - -120, height / 2 - 100, 70, 50);
-  }
-  if (this.key3.width > 0 && this.key3.height > 0) {
-    image(this.key3, width / 2 - 200, height / 2 - 100, 70, 50);
-  }
-  if (this.key4.width > 0 && this.key4.height > 0) {
-    image(this.key4, width / 2 - 250, height / 2 - 190, 70, 50);
-  }
-  if (this.monsterPurple.width > 0 && this.monsterPurple.height > 0) {
-    image(this.monsterPurple, width / 2 - -130, height / 2 - -40, 50, 50);
-  }
-  if (this.monsterBlue.width > 0 && this.monsterBlue.height > 0) {
-    image(this.monsterBlue, width / 2 - 200, height / 2 - -40, 50, 50);
-  }
- 
+  image(this.yellowSnowman, width / 2, this.y + this.height * 0.25, this.width * 0.12, this.width * 0.20);
+  image(this.greenSnowman, width / 2, this.y + this.height * 0.75,  this.width * 0.12, this.width * 0.20);
+  image(this.key, this.x + this.width * 0.1, this.y + this.height * 0.1, this.width * 0.1, this.width * 0.05);
+  image(this.key2, this.x + this.width * 0.2, this.y + this.height * 0.2, this.width * 0.1, this.width * 0.05);
+  image(this.key3, this.x + this.width * 0.8, this.y + this.height * 0.2, this.width * 0.1, this.width * 0.05);
+  image(this.key4, this.x + this.width * 0.9, this.y + this.height * 0.1, this.width * 0.1, this.width * 0.05);
+  image(this.monsterPurple, this.x + this.width * 0.25, height / 2 + this.height * 0.1, this.width * 0.1, this.width * 0.12);
+  image(this.monsterBlue, this.x + this.width * 0.75, height / 2 + this.height * 0.1, this.width * 0.1, this.width * 0.12);
+  pop();
 }
 
   // Creates the buttons with a for loop based on the options array length
   private drawButtons() {
     for (let i = 0; i < this.options.length; i++) {
       let button = createButton(this.options[i]);
-      button.position(width / 1.6, height / 1.4 + (i + 1) * 50);
+      button.position(this.x + this.width - 170, this.y + this.height - 160 + (i + 1) * 50);
       button.size(150, 40);
       // button.center("horizontal");
       button.style("color: #4A7AA7");
@@ -139,7 +120,6 @@ public drawImages() {
     for (let i = 0; i < this.options.length; i++) {
       if (i === this.currentOption) {
         this.buttons[i].style("background-color", "rgb(100, 190, 230)");
-        console.log("Hover effect");
       } else {
         this.buttons[i].style("background-color", "#D2ECF3");
       }
